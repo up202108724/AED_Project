3fba6e1e3d8b517f6160cf59b45dc65d2e15b0a7
André Silva <up202108724@g.uporto.pt>
2022-11-06 12:29:19 +0000

    Complexidades Joao

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/andre/Desktop/projetofinal
+# Install script for directory: /home/andre/Desktop/AED_Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/andre/Desktop/projetofinal/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/andre/Desktop/AED_Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
